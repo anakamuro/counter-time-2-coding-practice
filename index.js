0734bed886b267f7a6a842565dbb215c53fffea9
Akihiko Nakamura <anakamuro@earthlink.net>
2023-12-30 11:27:07 -0500

    commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const endDate = document.querySelector("input[name='endDate']")
+console.log(endDate)
+const clock = document.querySelector(".clock");
+let timeInterval;
+let timeStop = true;
+const savedValue = localStorage.getItem("countdown")
+if(savedValue){
+    startClock(savedValue);
+    let inputValue = new Date(savedValue);
+    console.log(inputValue);
+    endDate.valueAsDate = inputValue;
+}
+endDate.addEventListener("change", function(e){
+    e.preventDefault();
+    clearInterval(timeInterval)
+    const temp = new Date(endDate.value);
+    localStorage.setItem("countdown", temp);
+    startClock(temp)
+    timeStop = false;
+})
+
+function startClock(d){
+function updateCounter(){
+    let tl = (timeLeft(d));
+    if(tl.total <= 0){
+        timeStop = false;
+    }
+    console.log(tl.days);
+    for(let pro in tl){
+        console.log(pro, tl[pro]);
+        let el = clock.querySelector("."+ pro);
+        if(el){
+            console.log(el)
+            el.innerHTML = tl[pro]
+        }
+    }
+    // clock.innerHTML = tldays + " " + tl.hours;
+}
+updateCounter();
+if(timeStop){
+    timeInterval = setInterval (updateCounter, 1000)
+} else {
+    clearInterval(timeInterval)
+}
+}
+
+function timeLeft(d){
+   let currentDate = new Date();
+//    console.log(Date.parse(d));
+//    console.log(currentDate)
+//    console.log(Date.parse(currentDate));
+   let t = Date.parse(d) - Date.parse(currentDate);
+   let seconds = Math.floor((t / 1000) % 60);
+   let minutes = Math.floor((t / 1000 / 60) % 60);
+   let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+   let days = Math.floor(t / (1000 * 60 * 60 * 24));
+   return {
+    "total": t, 
+    "days": days,
+    "hours": hours,
+    "minutes": minutes, 
+    "seconds": seconds
+   }
+   
+}
